ddbb4e5b0faa9e84858c57dfff8c1b81a646c36c
kuma <kumaleta2021@gmail.com>
2024-04-17 10:16:00 +0300

    home page update

--- Desktop/TypeScript/note_taking_app/front-end/src/components/home.tsx
+++ Desktop/TypeScript/note_taking_app/front-end/src/components/home.tsx
@@ -4,9 +4,20 @@ import "../styles/home.css";
 const Home: React.FC = () => {
   return (
     <>
-      <h1> home page</h1>
-      <Link to="/signup">signup</Link>
-      <Link to="/login">login </Link>
+      <div className="home_container">
+        <div className="home_left">
+          <h3> SIMPLIFY YOUR NOTE TAKING </h3>
+          <h1>BOOST YOUR PRODUCTIVITY WITH OUR NOTE TAKING WEB APP</h1>
+          <h2>START ORGANIZING YOUR NOTES TODAY</h2>
+          <div className="links_container">
+            <Link to="/signup">signup</Link>
+            <Link to="/login">login </Link>
+          </div>
+        </div>
+        <div className="noteImage">
+          <img src="note_image.jpg" alt="no image" />
+        </div>
+      </div>
     </>
   );
 };
